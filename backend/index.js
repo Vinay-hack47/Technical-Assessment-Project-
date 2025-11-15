3929644335b91bf6d0982958803d22ff541a3cbd
Rajput Vinaykumar <149587018+Vinay-hack47@users.noreply.github.com>
2025-11-15 14:32:09 +0530

    Enhance CORS settings with methods and headers
    Added allowed methods and headers to CORS configuration.

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,11 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: "https://technical-assessment-project-8fje.vercel.app",
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 app.listen(PORT, () => {
   console.log("🚀 Backend running on port", PORT);
