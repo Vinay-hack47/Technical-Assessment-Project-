cff7e41f008bcdb5b4907eef53433d4883c962ad
Vinay-hack47 <rajputvinay1212@gmail.com>
2025-11-15 07:46:48 +0530

    error resolved

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}api/v1/user/login`, userData, {
+      const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/user/login`, userData, {
         headers: {
           "Content-Type": "application/json"
         },
