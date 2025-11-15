f686934f4ebb6c4396008dfde62218e5054971b5
Rajput Vinaykumar <149587018+Vinay-hack47@users.noreply.github.com>
2025-11-15 15:18:51 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,29 +7,39 @@ import path from "path";
 import userRouter from "./router/user.route.js";
 import ocrRoutes from "./router/ocrRoutes.js";
 import ocrAnalyzeRoutes from "./router/ocrAnalyzeRoutes.js";
-import serverless from "serverless-http";
+
 
 dotenv.config();
 connectDB();
 
 const app = express();
+const PORT=5000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // FIXED: remove trailing slash
-app.use(cors({
-  origin: "https://technical-assessment-project-8fje.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://technical-assessment-project-8fje.vercel.app",
+];
 
-app.options("*", cors());
+// FIXED: Proper CORS config
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
+
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/ocr", ocrRoutes);
 app.use("/api/v1/ocr-analyzer", ocrAnalyzeRoutes);
 
-export default serverless(app);
+
+app.listen(PORT, () => {
+  console.log("🚀 Backend running on port", PORT);
+});
