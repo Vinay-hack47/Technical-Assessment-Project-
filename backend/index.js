0408f6434dd0e1e20200dd94a762578e266a977e
Rajput Vinaykumar <149587018+Vinay-hack47@users.noreply.github.com>
2025-11-15 14:20:48 +0530

    Implement CORS with allowed origins
    Added CORS configuration to allow specific origins.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
+const allowedOrigins = [
+  
+  "https://technical-assessment-project-8fje.vercel.app"
+];
+
+// FIXED: Proper CORS config
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 
 app.listen(PORT, () => {
   console.log("🚀 Backend running on port", PORT);
