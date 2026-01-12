aa87e5d129e5a2191006958d8cbd1c4d4849e56f
Vinay-hack47 <rajputvinay1212@gmail.com>
2026-01-12 14:42:45 +0530

    index.js update

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,49 @@
-import app from "./app.js";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./db/connectDB.js";
+import path from "path";
+import userRouter from "./router/user.route.js";
+import ocrRoutes from "./router/ocrRoutes.js";
+import ocrAnalyzeRoutes from "./router/ocrAnalyzeRoutes.js";
 
-const PORT = process.env.PORT || 3000;
-app.use(cors({
-  origin: "https://technical-assessment-project-8fje.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+dotenv.config();
+connectDB();
+
+const app = express();
+const PORT=5000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// FIXED: remove trailing slash
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://technical-assessment-project-8fje.vercel.app",
+];
+
+// FIXED: Proper CORS config
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
+// app.use(cors({
+//   origin: "https://technical-assessment-project-8fje.vercel.app",
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// }));
+
+
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/ocr", ocrRoutes);
+app.use("/api/v1/ocr-analyzer", ocrAnalyzeRoutes);
 
 
 app.listen(PORT, () => {
